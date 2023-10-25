4259d9256882e35a4074656a0f64e71086687705
masud1989 <mranasrj@gmail.com>
2023-10-25 16:51:43 +0600

    fetch-data

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from '../components/Header';
 
-const page = () => {
+const page = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json()
+
   return (
    <>
       <Header />
       <div>Blog Page</div>
+      {
+        data.map( (item) =>{
+          <h1>{item.title}</h1>
+          // <p>{item.body}</p>
+        } )
+      }
    </>
   )
 }
